1914639ef773e71f38f82cd7b739559806bccdb4
yvnalv <yvnalv@gmail.com>
2024-09-10 22:37:15 +0700

    Create partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';  // For resolving __dirname
 import routes from './routes/index.js';  // Import routes (need .js extension for ES module)
+import expressLayouts from 'express-ejs-layouts';
 
 // Create an instance of express
 const app = express();
@@ -13,6 +14,7 @@ const __dirname = path.dirname(__filename);
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);  // Add this line
 
 // Serve static files
 app.use(express.static('public'));
